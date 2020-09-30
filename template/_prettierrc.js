cbb7fa6c03ee33c0f33b80ca06bb97d805244877
youngjuning <youngjuning@aliyun.com>
2020-09-30 18:15:59 +0800

    chore: 删除注释

--- template/_prettierrc.js
+++ template/_prettierrc.js
@@ -1,39 +1,12 @@
 module.exports = {
-  printWidth: 150, // 一行的字符数，如果超过会进行换行，默认为80
-  tabWidth: 2, // tab缩进大小,默认为2
-  useTabs: false, // 使用tab缩进，默认false
-  semi: true, // 使用分号, 默认true
-  /**
-   * 行尾逗号,默认none,可选 none|es5|all
-   * es5 包括es5中的数组、对象
-   * all 包括函数对象等所有可选
-   */
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   trailingComma: "all",
-  singleQuote: true, // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
-  /**
-   * 对象中的空格 默认true
-   * true: { foo: bar }
-   * false: {foo: bar}
-   */
+  singleQuote: true,
   bracketSpacing: true,
-  /**
-   * JSX标签闭合位置 默认false
-   * false:
-   * <div
-   *  className=""
-   *  style={{}}
-   * >
-   * true:
-   * <div
-   *  className=""
-   * style={{}} >
-   */
   jsxBracketSameLine: false,
-  /**
-   * 箭头函数参数括号 默认avoid 可选 avoid| always
-   * avoid 能省略括号的时候就省略 例如x => x
-   * always 总是有括号
-   */
   arrowParens: "avoid",
-  endOfLine: "lf", // 强制使用 unix 风格的换行符
+  endOfLine: "lf",
 };
